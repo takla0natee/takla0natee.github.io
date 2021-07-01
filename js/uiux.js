9ea7840d32742e478d1abf374036f1efcb67f8fb
takla0natee <37974342+takla0natee@users.noreply.github.com>
2021-07-01 14:46:58 +0700

    beautified UI

---
+++ js/uiux.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var x = document.getElementById("menu-items");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+}
